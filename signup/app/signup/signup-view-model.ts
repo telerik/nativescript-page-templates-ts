4b5d91d6883dffc713a3bfc2b116b76114864fe5
Alexander Djenkov <djenkov@progress.com>
2017-07-25 14:10:08 +0300

    Update signup and make SASS ready

--- signup/app/signup/signup-view-model.ts
+++ signup/app/signup/signup-view-model.ts
@@ -1,47 +1,18 @@
-import { Observable } from 'data/observable';
+import { Observable } from "data/observable";
+import { ObservableProperty } from "./observable-property-decorator";
 
 export class SignupViewModel extends Observable {
-    private _name: string;
-    private _email: string;
-    private _password: string;
+    @ObservableProperty() password: string;
+    @ObservableProperty() name: string;
+    @ObservableProperty() email: string;
 
     constructor() {
         super();
     }
 
-    get name(): string {
-        return this._name;
-    }
-
-    get email(): string {
-        return this._email;
-    }
-
-    get password(): string {
-        return this._password;
-    }
-
-    set name(value: string) {
-        if (this._name !== value) {
-            this._name = value;
-            this.notifyPropertyChange('name', value)
-        }
-    }
-
-    set email(value: string) {
-        if (this._email !== value) {
-            this._email = value;
-            this.notifyPropertyChange('email', value)
-        }
-    }
-
-    set password(value: string) {
-        if (this._password !== value) {
-            this._password = value;
-            this.notifyPropertyChange('password', value)
-        }
-    }
-
+    /* ***********************************************************
+    * TODO: Implement signin.
+    *************************************************************/
     signup() {
     }
 }
