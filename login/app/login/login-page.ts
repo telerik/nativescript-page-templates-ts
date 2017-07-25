6a023000dde5c13451d7a07d6c246c0f141b0bea
Alexander Djenkov <djenkov@progress.com>
2017-07-25 14:12:41 +0300

    Remove unnecessary comments

--- login/app/login/login-page.ts
+++ login/app/login/login-page.ts
@@ -1,15 +1,3 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
-
-/*
-NativeScript adheres to the CommonJS specification for dealing with
-JavaScript modules. The CommonJS require() function is how you import
-JavaScript modules defined in other files.
-*/
-
 import { EventData } from "data/observable";
 import { Page } from "ui/page";
 import { LoginViewModel } from "./login-view-model";
