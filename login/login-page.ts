2435c46650c18db4406a0ace79b84ef3bb221988
Manol Donev <manol.donev@gmail.com>
2019-08-07 18:26:35 +0300

    chore: update short imports

--- login/login-page.ts
+++ login/login-page.ts
@@ -1,6 +1,6 @@
-import { EventData } from "data/observable";
-import { Button } from "ui/button";
-import { Page } from "ui/page";
+import { EventData } from "tns-core-modules/data/observable";
+import { Button } from "tns-core-modules/ui/button";
+import { Page } from "tns-core-modules/ui/page";
 
 import { <%= PascalCaseName %>ViewModel } from "./<%= OriginalName %>-view-model";
 
